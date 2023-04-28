29bb41fab1e5cbf45d9ed2f775a7bd77fe9c9c42
Nouhaila <iguerdnouhaila@gmail.com>
2023-04-28 23:08:08 +0100

    adding the pad with numbers

--- test_your_brain/lib/utils/color.dart
+++ test_your_brain/lib/utils/color.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class MyColors {
   static const Color myColor = Color(0xFF7497CA);
+  static const Color boxColor = Color.fromARGB(255, 81, 106, 143);
 }
---
+++ test_your_brain/lib/utils/text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SmallTextStyle {
+  static const smallTextStyle = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 40,
+    color: Colors.white,
+  );
+}
