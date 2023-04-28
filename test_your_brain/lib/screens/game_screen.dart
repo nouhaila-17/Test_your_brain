0cb38a36c900557a356567410c4a460f7c31ba3e
Nouhaila <iguerdnouhaila@gmail.com>
2023-04-28 21:16:37 +0100

    adding the start_screen and working button to game screen

---
+++ test_your_brain/lib/screens/game_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  //route
+  static String routeName = '/game-screen';
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- test_your_brain/lib/screens/start_screen.dart
+++ test_your_brain/lib/screens/start_screen.dart
@@ -1,16 +1,72 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatefulWidget {
+import '../utils/color.dart';
+
+class StartScreen extends StatelessWidget {
   static String routeName = '/start-game-screen';
   const StartScreen({super.key});
 
-  @override
-  State<StartScreen> createState() => _StartScreenState();
-}
+  //function
+  void startGame(BuildContext context) {
+    Navigator.pushNamed(context, routeName);
+  }
 
-class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: MyColors.myColor,
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          //space
+          children: [
+            Container(
+              height: 200,
+            ),
+            SizedBox(
+              height: 100,
+              width: 350,
+              child: Image.asset('images/logo_image.jpg'),
+            ),
+            //space
+            const SizedBox(height: 100),
+            //text
+            const Center(
+              child: SizedBox(
+                width: 300,
+                child: Text(
+                  'In this game you will solve 20 mathematic operations\nthe maximum time to solve each operation is 10s\nin the end you will know your score on 20',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+
+            //space
+            const SizedBox(height: 50),
+            //button
+            ElevatedButton(
+                onPressed: () => startGame(context),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                      // fontSize: 18,
+                      // fontWeight: FontWeight.bold,
+                      ),
+                  minimumSize: const Size(200, 50),
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    color: MyColors.myColor,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+          ],
+        ));
   }
 }
