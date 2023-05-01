4b587319cfe99921c6a074ac25c00ac3226af32e
fadouamaouli <130999598+fadouamaouli@users.noreply.github.com>
2023-05-01 15:11:47 +0200

    fadoua changes

--- test_your_brain/lib/screens/game_over_screen.dart
+++ test_your_brain/lib/screens/game_over_screen.dart
@@ -1,83 +1,261 @@
 import 'package:flutter/material.dart';
+import 'package:test_your_brain/screens/start_screen.dart';
+import 'package:test_your_brain/styles/color.dart';
+import 'package:test_your_brain/utils/pad_buttons.dart';
+import 'package:test_your_brain/styles/text_styles.dart';
+import 'package:test_your_brain/screens/final_screen.dart';
+import '../utils/errors_handle.dart';
+import '../utils/random_operators.dart';
 
-class GameOverScreen extends StatelessWidget {
-  final int score;
-  final int level;
+class GameScreen extends StatefulWidget {
+  //route
+  static String routeName = '/game-screen';
+  const GameScreen({super.key});
 
-  GameOverScreen({required this.score, required this.level});
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
 
+class _GameScreenState extends State<GameScreen> {
+  int score = 0;
+
+  //numbers of the pad
+  List<String> numberPad = [
+    '7',
+    '8',
+    '9',
+    'AC',
+    '4',
+    '5',
+    '6',
+    'DEL',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '',
+    '',
+    '='
+  ];
+  //operation////////////////////////////////////
+  RandomOperations randomOperators = RandomOperations();
+  //user's answer////////////
+  String userAnswer = '';
+  //tapped button/////////////////////////////////
+  void tappedButton(String clicked) {
+    setState(() {
+      if (clicked == '=') {
+        //seing if the user is correct
+        checkResult();
+      } else if (clicked == '') {
+        userAnswer;
+      } else if (clicked == 'DEL') {
+        if (userAnswer == '') {
+          userAnswer = '';
+        } else {
+          userAnswer = userAnswer.substring(0, userAnswer.length - 1);
+        }
+        //deliting the last number only
+      } else if (clicked == 'AC') {
+        //deleting everything
+        userAnswer = '';
+      } else {
+        //max numbers ton enter
+        if (userAnswer.length < 3) {
+          userAnswer += clicked;
+        }
+      }
+    });
+  }
+
+//checking the results///////////////////////////////////
+  int checkResult() {
+    int userAnswerInt = int.tryParse(userAnswer) ?? 0;
+    if (userAnswerInt == randomOperators.getCorrectAnswer()) {
+      score += 1;
+      // user is correct
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Container(
+                height: 200,
+                color: Colors.green,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      'Correct answer',
+                      style: SmallTextStyle.smallTextStyle,
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          nextOperation(), // move to next operation immediately
+                      child: Container(
+                        height: 36,
+                        width: 36,
+                        decoration: BoxDecoration(
+                          color: Colors.green[300],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                    //button to the next operation
+                  ],
+                ),
+              ),
+            );
+          });
+
+      //score
+    } else {
+      //// if the answer is incorrect, keep the current operation and reset the user's answer
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Container(
+                height: 200,
+                color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      'Wrong answer',
+                      style: SmallTextStyle.smallTextStyle,
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          nextOperation(), // move to next operation immediately
+                      child: Container(
+                        height: 36,
+                        width: 36,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 217, 11, 11),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                    //button to the next operation
+                  ],
+                ),
+              ),
+            );
+          });
+    }
+    return score;
+  }
+
+  int i = 0;
+  //next question function
+  void nextOperation() {
+    //removing dialog
+    Navigator.of(context).pop();
+    //reset operation and userAnswer
+    setState(() {
+      randomOperators = RandomOperations();
+      userAnswer = '';
+    });
+    ++i;
+    Cond();
+  }
+
+  void Cond() {
+    if (i == 20) {
+      Navigator.pushNamed(
+        context,
+        FinalScreen.routeName,
+        arguments: {'score': score},
+      );
+    }
+  }
+
+//function
+  //void EndOfGame(BuildContext context) {
+  //   Navigator.pushNamed(context, FinalScreen.routeName);
+  // }
+
+  //////////////////////////The SCAFFOLD ///////////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MyColors.myColor,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Test Your Brain',
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
+        ),
+        elevation: 10,
+        backgroundColor: Color.fromARGB(255, 3, 22, 98),
+        leading: Container(
+          child: Image.asset('images/logo_image.jpg'),
+        ),
+      ),
       body: Column(
         children: [
-          // Free space at the top of the interface
-          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+          //logo and Timer////////////////////////////////////////////////
 
-          // Space for the logo
-          Image.asset(
-            'logo.png.jpg',
-            height: 100,
-            width: 100,
-          ),
-
-          // "The Game is over" message
-          SizedBox(height: 20),
-          Text(
-            'The Game is over',
-            style: TextStyle(fontSize: 24),
-          ),
-
-          // Rectangular container for final score
-          SizedBox(height: 20),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Text(
-              'Your final score is: $score',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-
-          // Rectangular container for level
-          SizedBox(height: 20),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Text(
-              'Your level is: $level',
-              style: TextStyle(fontSize: 20),
+          //operation//////////////////////////////////////////////////////
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //operation
+                  Text(
+                    '${randomOperators.firstNumber}${randomOperators.operator}${randomOperators.secondNumber}= ',
+                    style: NumberTextStyle.numberTextStyle,
+                  ),
+                  //answer
+                  SizedBox(
+                    //color: Colors.amber,
+                    height: 60,
+                    width: 90,
+                    child: Text(
+                      userAnswer,
+                      style: NumberTextStyle.numberTextStyle,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
 
-          // Rectangular container for "Play again?" button
-          SizedBox(height: 20),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(10),
+          //buttons of numbers +delete + submit answer///////////////////////////
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: GridView.builder(
+                  itemCount: numberPad
+                      .length, //number of the boxes also is indexed from 0 to 11
+                  //physics:
+                  //  const NeverScrollableScrollPhysics(), //because it scrolls
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4, //3colomns
+                  ),
+                  itemBuilder: (context, index) {
+                    return NumberButton(
+                      child: numberPad[index],
+                      onTap: () => tappedButton(numberPad[index]),
+                    );
+                  }),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Play again?',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Icon(Icons.arrow_forward_ios),
-              ],
-            ),
-          ),
+          )
         ],
       ),
     );
   }
 }
-//comment
