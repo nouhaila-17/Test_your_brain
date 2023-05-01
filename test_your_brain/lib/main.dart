ed55ce09dec9dd64c17c10d6c2fad43205a79f9c
fadouamaouli <130999598+fadouamaouli@users.noreply.github.com>
2023-05-01 15:53:54 +0200

    fadoua changes

--- test_your_brain/lib/main.dart
+++ test_your_brain/lib/main.dart
@@ -5,6 +5,11 @@ import 'package:test_your_brain/screens/start_screen.dart';
 import 'package:test_your_brain/screens/welcome_page.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:test_your_brain/screens/final_screen.dart';
+
+
+import 'package:test_your_brain/styles/color.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -42,6 +47,11 @@ class MyApp extends StatelessWidget {
         WelcomePage.routeName: (context) => const WelcomePage(),
         StartScreen.routeName: (context) => const StartScreen(),
         GameScreen.routeName: (context) => const GameScreen(),
+
+        FinalScreen.routeName:(context) => const FinalScreen(),
+
+//        FinalScreen.routeName: (context) => const FinalScreen(),
+
       },
      // initialRoute: FinalScreen.routeName,
     );
